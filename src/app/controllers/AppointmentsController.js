afaecf3fcf7d7063f11dbd4e96e587052c3d9c2d
Douglas Morais <mr.douglasmorais23@gmail.com>
2021-08-20 14:49:49 -0300

    Aula 05

--- src/app/controllers/AppointmentsController.js
+++ src/app/controllers/AppointmentsController.js
@@ -1,17 +1,82 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
 import pt from 'date-fns/locale/pt-BR';
-
 import User from '../models/User';
-import Appointment from '../models/Appointments';
+import Appointment from '../models/Appointment';
 
 class AppointmentsController {
   async index(req, res) {
-    return res.status(200).json({ message: 'sem agendamento até o momento' })
+    const { page = 1 } = req.query;
+
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userID, canceled_at: null },
+      order: ['date'],
+      attributes: ['id', 'date'],
+      limit: 3,
+      offset: (page -1) * 3,
+      include: [
+        {
+          model: User,
+          as: 'collaborator',
+          attributes: ['id', 'name'],
+        }
+      ]
+    })
+
+    return res.status(200).json({ register: page,
+      appointments: appointments
+    });
   }
 
   async store( req, res) {
-    const { name } = req.body;
-    return res.status(200).json(name)
+    const schema = Yup.object().shape({
+      collaborator_id: Yup.number().required(),
+      date: Yup.date().required(),
+    })
+
+    if(!(await schema.isValid(req.body))){
+      return res.status(400).json({ message: 'Dados inválidos'})
+    }
+
+    const { collaborator_id, date } = req.body;
+
+    const isCollaborator = await User.findOne({
+      where: { id: collaborator_id, provider: true }
+    });
+
+    if(!isCollaborator){
+      return res.status(401).json({ error: 'Colaborador não encontrado'})
+    }
+
+    const startHour = startOfHour(parseISO(date));
+
+    if(isBefore(startHour, new Date())){
+      return res.status(400).json({
+        erro: 'Horário não disponível'
+      })
+    }
+
+    const checkAvaiabily = await Appointment.findOne({
+      where: {
+        collaborator_id,
+        canceled_at: null,
+        date: startHour,
+      }
+    })
+
+    if (checkAvaiabily){
+      return res.status(400).json({
+        message: 'Sem horário disponível'
+      })      
+    }
+    
+    const appointment = await Appointment.create({
+      user_id: req.userID,
+      collaborator_id,
+      date: startHour,
+    })
+
+    return res.status(200).json(appointment);
   }
 }
 
